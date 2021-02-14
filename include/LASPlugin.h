18db4cddccb6a6aa66a683a27c82b9f1fcd20c25
tmontaigu <thomas.montaigu@laposte.net>
2021-02-14 13:54:38 +0100

    Cleanups

--- include/LASPlugin.h
+++ include/LASPlugin.h
@@ -1,6 +1,6 @@
 //##########################################################################
 //#                                                                        #
-//#          CLOUDCOMPARE PLUGIN: ExampleIOPlugin                          #
+//#          CLOUDCOMPARE PLUGIN: LasIO                                    #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #
@@ -19,29 +19,17 @@
 
 #include "ccIOPluginInterface.h"
 
-/** Replace 'ExampleIOPlugin' by your own plugin class name throughout and then
-        check 'ExampleIOPlugin.cpp' for more directions.
 
-        Each plugin requires an info.json file to provide information about itself -
-        the name, authors, maintainers, icon, etc..
-
-        The one method you are required to implement is getFilters(). This method
-        registers your file I/O methods with CloudCompare.
-**/
-
-//! Example I/O Plugin
 class LASPlugin : public QObject, public ccIOPluginInterface
 {
     Q_OBJECT
     Q_INTERFACES(ccPluginInterface ccIOPluginInterface)
 
-    Q_PLUGIN_METADATA(IID "cccorp.cloudcompare.plugin.ExampleIO" FILE "../info.json")
+    Q_PLUGIN_METADATA(IID "cccorp.cloudcompare.plugin.LASIO" FILE "../info.json")
 
   public:
     explicit LASPlugin(QObject *parent = nullptr);
     ~LASPlugin() override = default;
-
-    void registerCommands(ccCommandLineInterface *cmd) override;
 
     // Inherited from ccIOPluginInterface
     ccIOPluginInterface::FilterList getFilters() override;
--- src/LASPlugin.cpp
+++ src/LASPlugin.cpp
@@ -1,6 +1,6 @@
 //##########################################################################
 //#                                                                        #
-//#          CLOUDCOMPARE PLUGIN: ExampleIOPlugin                          #
+//#          CLOUDCOMPARE PLUGIN: LasIO                                    #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #
@@ -15,22 +15,6 @@
 //#                                                                        #
 //##########################################################################
 
-// First:
-//	Replace all occurrences of 'ExampleIOPlugin' by your own plugin class name in this file.
-//	This includes the resource path to info.json in the constructor.
-
-// Second:
-//	Open ExampleIOPlugin.qrc, change the "prefix" and the icon filename for your plugin.
-//	Change the name of the file to <yourPluginName>.qrc
-
-// Third:
-//	Open the info.json file and fill in the information about the plugin.
-//	 "type" should be: "I/O"
-//	 "name" is the name of the plugin (required)
-//	 "icon" is the Qt resource path to the plugin's icon (from the .qrc file)
-//	 "description" is used as a tootip if the plugin has actions and is displayed in the plugin dialog
-//	 "authors", "maintainers", and "references" show up in the plugin dialog as well
-
 #include "LASPlugin.h"
 
 #include "LASIOFilter.h"
@@ -39,15 +23,6 @@ LASPlugin::LASPlugin(QObject *parent) : QObject(parent), ccIOPluginInterface(":/
 {
 }
 
-void LASPlugin::registerCommands(ccCommandLineInterface *cmd)
-{
-    Q_UNUSED(cmd);
-
-    // If you want to register this plugin for the command line, create a
-    // ccCommandLineInterface::Command and add it here. e.g.:
-    //
-    // cmd->registerCommand( ccCommandLineInterface::Command::Shared( new FooCommand ) );
-}
 
 ccIOPluginInterface::FilterList LASPlugin::getFilters()
 {
